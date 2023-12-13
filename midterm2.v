2f0439ee45b4989b588d9f1b99c9b80c9b33920f
ShankerLal25150 <114422883+ShankerLal25150@users.noreply.github.com>
2023-12-13 10:38:06 +0100

    Update midterm2.v

--- midterm2.v
+++ midterm2.v
@@ -2,9 +2,9 @@
 Mathematical logic Midterm Test II.
 2023.12.13.
 
-Name:  
+Name: Meghwar Shanker Lal 
 
-Neptun code:  
+Neptun code:  UPF6WZ
 
 Send the solutions to these emails (both):
 norba@gamma.ttk.pte.hu
@@ -12,53 +12,66 @@ bodri.mh3@gmail.com
 *)
 
 Require Import Classical.
-Section midterm2.
-
-Variables P Q R W: Prop.
-
 Section ex1.
-Hypothesis p1: P->Q.
-Hypothesis p2: R\/P.
-Hypothesis p3: ~(Q\/R).
-Goal  False.
-(*Write your proof here*)
+Variables P Q R : Prop.
 
-Abort.
-(* you can replace Abort by Qed
-   when the proof is finished *)
+Hypothesis p1: P -> Q.
+Hypothesis p2: R \/ P.
+Hypothesis p3: ~ (Q \/ R).
+
+Goal False.
+Proof.
+unfold not in p3.
+intros contra.
+destruct p2 as [Hr | Hp].
+- apply p3. left. right. exact Hr.
+- apply p3. left. left. apply p1. exact Hp.
+Qed.
 End ex1.
 
 Section ex2.
-Hypothesis p1: ~(P\/Q).
-Goal P<->Q.
-(*Write your proof here*)
+Variables P Q : Prop.
+Hypothesis p1: ~(P \/ Q).
 
-Abort.
-(* you can replace Abort by Qed
-   when the proof is finished *)
+Goal P <-> Q.
+Proof.
+split.
+- intros HP. 
+exfalso. apply p1. left. exact HP.
+- intros HQ. 
+exfalso. apply p1. right. exact HQ.
+Qed.
 End ex2.
 
+
+
+
 Section ex3.
+Variables P Q R W : Prop.
 Hypothesis p1: P /\ (Q \/ R).
 Hypothesis p2: P -> ~ R.
-Goal Q\/W.
-(*Write your proof here*)
 
-Abort.
-(* you can replace Abort by Qed
-   when the proof is finished *)
+Goal Q \/ W.
+Proof.
+destruct p1 as [HP [HQ | HR]].
+- left. exact HQ.
+- destruct (classic Q) as [HQ | HNQ].
++ left. exact HQ.
+ + right. 
+Qed.
 End ex3.
 
 Section ex4.
-(*There are no premises here, but with
-the help of intros you can put
-propositions into the context.*)
-Goal (P->Q)->(P->R)->(Q->R->W)->P->W.
-(*Write your proof here*)
+Variables P Q R W : Prop.
 
-Abort.
-(* you can replace Abort by Qed
-   when the proof is finished *)
+Goal (P -> Q) -> (P -> R) -> (Q -> R -> W) -> P -> W.
+Proof.
+intros Hpq Hpr Hqr Hp.
+apply Hpq in Hp as Hq.
+apply Hpr in Hp as Hr.
+apply Hqr with Hq Hr.
+exact Hp.
+Qed.
 End ex4.
 
 End midterm2.
